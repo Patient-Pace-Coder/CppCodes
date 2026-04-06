58d3bd2298bb1855e041d89886095f40b34215c7
Patient_Pace_Coder <104113247+Patient-Pace-Coder@users.noreply.github.com>
2026-04-06 19:08:10 +0530

    Uncomment code and added some necessary comments

--- 08_Namespaces.cpp
+++ 08_Namespaces.cpp
@@ -1,19 +1,20 @@
-// #include<iostream>
-// #include "circle.h"; // this allows us to use the variables and functions defined in this header file
-// #include "growth.h"; // this allows us to use the variables and functions defined in this header file
-// using namespace std;
+#include<iostream>
+#include "circle.h"; // this allows us to use the variables and functions defined in this header file
+#include "growth.h"; // this allows us to use the variables and functions defined in this header file
+using namespace std;
 
-// void foo(){
-//     cout << "I M FOO" << endl;
-// }
+void foo(){
+    cout << "I M FOO" << endl;
+}
 
-// int main(){
+int main(){
 
-//     // cout << basicMaths :: pi << endl;
-//     // cout << basicMaths :: e << endl;
+    cout << basicMaths :: pi << endl; // pi is defined inside circle.h but inside basicMaths namespace.
+    cout << basicMaths :: e << endl; // e is defined inside growth.h but inside basicMaths namespace.
 
-//     // the way header files circle and growth are in the same namespace basicMaths, similary every stl i.e. vector,hashmap..etc in written in namespace std
-// }
+    // the way header files circle.h and growth.h are using the same namespace basicMaths, similary every stl i.e. vector,hashmap..etc in written in namespace std.
+    // so whatever code is written inside same namespace is considered as one part.
+}
 
 
 
