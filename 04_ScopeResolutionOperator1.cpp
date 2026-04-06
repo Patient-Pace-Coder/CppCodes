2e88b3db1e2716f014465c455d00ff29df6e4948
Patient_Pace_Coder <104113247+Patient-Pace-Coder@users.noreply.github.com>
2026-04-06 18:56:32 +0530

    Uncomment code and fix namespace usage

--- 04_ScopeResolutionOperator1.cpp
+++ 04_ScopeResolutionOperator1.cpp
@@ -1,29 +1,29 @@
-// # include<iostream>
-// using namespace std;
+# include<iostream>
+using namespace std;
 
-// /*
-//  * Assume there are two files foo.cpp and goo.cpp and there's a similar method in both the files,now what if we call a method in our main.cpp file,
-//  * there's an ambuiguity for the compiler as it doesn't know which method to call,so we use the concept of namespaces
-//  */
+/*
+ * Assume there are two files foo.cpp and goo.cpp and there's a similar method in both the files,now what if we call a method in our main.cpp file,
+ * there's an ambuiguity for the compiler as it doesn't know which method to call,so we use the concept of namespaces
+ */
 
-// namespace Foo {
+namespace Foo {
 
-//     void doSomething(){
-//         cout << "There" << endl;
-//     }
-// }
+    void doSomething(){
+        cout << "There" << endl;
+    }
+}
 
-// namespace Goo{
+namespace Goo{
 
-//     void doSomething(){
-//         cout << "Hello" << endl;
-//     }
-// }
+    void doSomething(){
+        cout << "Hello" << endl;
+    }
+}
 
-//     int main(){
+    int main(){
 
-//         // doSomething(); // it results in runtime error
+        // doSomething(); // it results in runtime error
 
-//         Foo :: doSomething();
-//         Goo :: doSomething();
-//     }
+        Foo :: doSomething();
+        Goo :: doSomething();
+    }
